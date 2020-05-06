9ec4f33048b6afe6292918cbb4b10851a21fdd07
Nathan Carlin <carlin010@gannon.edu>
2020-05-06 15:36:26 -0400

    Initial setup of getting current user location

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Geolocation'),
-        ),
-        body: Center(
-          child: Text('Connected / Needs Configuration'),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: HomePage(),
     );
   }
 }
